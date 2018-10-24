5fef4f9d2290b0550533d2a8ad6dea88b23d2fb5
Srikrishna Pothukuchi <srikrishna@bcs.org.uk>
2018-10-24 18:54:14 +0530

    login automated, head code as well as headless

--- src/WebTest.java
+++ src/WebTest.java
@@ -1,19 +1,37 @@
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class WebTest {
 
 	public static void main(String[] args) {
 		System.setProperty("webdriver.gecko.driver", "./bin/geckodriver.exe");
+		
+		//headless mode
+		/*FirefoxBinary firefoxBinary = new FirefoxBinary(); 
+		firefoxBinary.addCommandLineOptions("--headless");
+		FirefoxOptions firefoxOptions = new FirefoxOptions();
+		firefoxOptions.setBinary(firefoxBinary);
+		FirefoxDriver driver = new FirefoxDriver(firefoxOptions);*/
+		
+		//head mode
 		WebDriver driver = new FirefoxDriver();
-		driver.get("http://store.demoqa.com");
+		
+		driver.get("http://facebook.com");
 		System.out.println("Successfully opened the website.");
 		
-		try {
-			Thread.sleep(5000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		driver.findElement(By.name("email")).sendKeys("dummy");
+		driver.findElement(By.name("pass")).sendKeys("dummy");
+		driver.findElement(By.id("loginbutton")).click();
 		
 		driver.quit();
 		System.out.println("closed...");
