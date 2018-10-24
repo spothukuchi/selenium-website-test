1b5dd38e51fc42a33e67280fe9995a6383a62f94
Srikrishna Pothukuchi <srikrishna@bcs.org.uk>
2018-10-24 17:04:50 +0530

    Workable frame

--- src/WebTest.java
+++ src/WebTest.java
@@ -1,9 +1,22 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class WebTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello Selenium");
+		System.setProperty("webdriver.gecko.driver", "./bin/geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://store.demoqa.com");
+		System.out.println("Successfully opened the website.");
+		
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		driver.quit();
+		System.out.println("closed...");
 	}
 
 }
